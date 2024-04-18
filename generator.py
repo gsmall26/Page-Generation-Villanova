1db5a4026acf123e009fb262e1c865843edd4272
gsmall26 <gsmall2@villanova.edu>
2024-04-18 12:20:49 -0400

    split up images appear as one

--- generator.py
+++ generator.py
@@ -1,27 +1,11 @@
-from flask import Flask, render_template, request
-import cv2
-import os
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    image_path = './image.png'
+    image_path = 'image.png'  # Path to your image file
     return render_template('index.html', image_path=image_path)
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if request.method == 'POST':
-#         # Capture snapshot using OpenCV
-#         camera = cv2.VideoCapture(0)
-#         _, image = camera.read()
-#         camera.release()
-
-#         # Save the snapshot
-#         cv2.imwrite('static/snapshot.jpg', image)
-        
-#         return render_template('result.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
