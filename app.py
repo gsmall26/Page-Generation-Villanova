5ed416a80a5a3ae60460b6d4339d223f08787178
gsmall26 <gsmall2@villanova.edu>
2024-09-30 11:33:50 -0400

    relative file paths

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def get_latest_cropped_face():
     latest_file = max(files, key=lambda x: int(x.split('_')[2].split('.')[0]))
     return latest_file
 
-@app.route('/')
+@app.route('/api/')
 def index():
     latest_cropped_face = get_latest_cropped_face()
     print(latest_cropped_face)
     return render_template('workingpage.html', latest_cropped_face=latest_cropped_face)
 
-@app.route('/faces/<filename>')
+@app.route('/api/faces/<filename>')
 def faces(filename):
     return send_from_directory(FACES_FOLDER, filename)
 
